689be920816bfd35d4550fd9abdb75ad887452dc
Rick Gilmore <rog1@psu.edu>
2022-11-22 11:19:37 -0500

    Switch to bootstrap4.

---
+++ _render.R
@@ -0,0 +1,19 @@
+render_protocol_bootstrap4 <- function(output_dir = 'docs') {
+  if (!dir.exists(output_dir)) {
+    stop("Output directory doesn't exist: ", output_dir)
+  }
+  
+  
+  
+  bookdown::render_book('protocol', 
+                        output_format = bookdown::bs4_book())
+}
+
+render_protocol_gitbook <- function(output_dir = 'docs') {
+  if (!dir.exists(output_dir)) {
+    stop("Output directory doesn't exist: ", output_dir)
+  }
+    
+  bookdown::render_book('protocol')
+  
+}

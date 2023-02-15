514d81bbeb62d7c7babcadfeeefe3d08c0e70b99
Rick Gilmore <rog1@psu.edu>
2023-02-15 10:17:22 -0500

    Separate data updating from report rendering.

---
+++ R/constants.R
@@ -0,0 +1,5 @@
+# r/protocol/constants.R
+
+report_dir <- 'protocol'
+google_sheet_fn <- 'C-ORR Survey 2022 Fall (Responses)'
+csv_fn <- file.path(report_dir, "csv/open-science-survey-2022-fall.csv")
